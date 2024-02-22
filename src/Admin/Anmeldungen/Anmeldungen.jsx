65e6f623d8a37ab68154fbd47582d756e1abff5e
Timo Blumer <timo.blumer@gewa.ch>
2024-02-22 19:32:00 +0100

    update: edit and delete mode

--- src/Admin/Anmeldungen/Anmeldungen.jsx
+++ src/Admin/Anmeldungen/Anmeldungen.jsx
@@ -7,6 +7,7 @@ const Anmeldungen = () => {
   const [trauungenData, setTrauungenData] = useState([]);
   const [showSummary, setShowSummary] = useState(false);
   const [editedAnmeldung, setEditedAnmeldung] = useState(null);
+  const [editedTrauung, setEditedTrauung] = useState(null);
 
   const fetchAnmeldungen = async () => {
     try {
@@ -18,17 +19,17 @@ const Anmeldungen = () => {
     }
   };
 
-  useEffect(() => {
-    const fetchTrauungen = async () => {
-      try {
-        const response = await axios.get('https://hochzeit-database-backend.onrender.com/api/v1/trauungen');
-        console.log('Trauungen Response data:', response.data);
-        setTrauungenData(response.data);
-      } catch (error) {
-        console.error('Error fetching Anmeldungen data:', error);
-      }
-    };
+  const fetchTrauungen = async () => {
+    try {
+      const response = await axios.get('https://hochzeit-database-backend.onrender.com/api/v1/trauungen');
+      console.log('Trauungen Response data:', response.data);
+      setTrauungenData(response.data);
+    } catch (error) {
+      console.error('Error fetching Trauungen data:', error);
+    }
+  };
 
+  useEffect(() => {
     fetchAnmeldungen();
     fetchTrauungen();
   }, []);
@@ -50,15 +51,37 @@ const Anmeldungen = () => {
     }
   };
 
+  const handleDeleteTrauung = async (trauungId) => {
+    const isConfirmed = window.confirm('Bist du sicher, dass du diese Trauung löschen möchtest?');
+
+    if (isConfirmed) {
+      try {
+        const response = await axios.delete(`https://hochzeit-database-backend.onrender.com/api/v1/trauungen/${trauungId}`);
+        console.log('Trauung erfolgreich gelöscht:', response.data);
+        // Daten erneut abrufen und aktualisieren
+        fetchTrauungen();
+      } catch (error) {
+        console.error('Fehler beim Löschen der Trauung:', error);
+      }
+    } else {
+      console.log('Löschen abgebrochen.');
+    }
+  };
+
   const handleEditAnmeldung = (anmeldung) => {
     setEditedAnmeldung(anmeldung);
   };
 
-  const handleCancelEdit = () => {
-    setEditedAnmeldung(null);
+  const handleEditTrauung = (trauung) => {
+    setEditedTrauung(trauung);
   };
 
-  const handleSaveEdit = async () => {
+  const handleCancelEdit = () => {
+    setEditedAnmeldung(null);
+    setEditedTrauung(null);
+  };
+
+  const handleSaveEditAnmeldung = async () => {
     try {
       const response = await axios.put(`https://hochzeit-database-backend.onrender.com/api/v1/anmeldungen/${editedAnmeldung.id}`, editedAnmeldung);
       console.log('Anmeldung erfolgreich aktualisiert:', response.data);
@@ -70,9 +93,29 @@ const Anmeldungen = () => {
     }
   };
 
-  const handleInputChange = (e) => {
+  const handleSaveEditTrauung = async () => {
+    try {
+      const response = await axios.put(`https://hochzeit-database-backend.onrender.com/api/v1/trauungen/${editedTrauung.id}`, editedTrauung);
+      console.log('Trauung erfolgreich aktualisiert:', response.data);
+      // Daten erneut abrufen und aktualisieren
+      fetchTrauungen();
+      setEditedTrauung(null); // Bearbeitungsmodus beenden
+    } catch (error) {
+      console.error('Fehler beim Aktualisieren der Trauung:', error);
+    }
+  };
+
+  const handleInputChangeAnmeldung = (e) => {
     const { name, value } = e.target;
     setEditedAnmeldung(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleInputChangeTrauung = (e) => {
+    const { name, value } = e.target;
+    setEditedTrauung(prevState => ({
       ...prevState,
       [name]: value
     }));
@@ -96,11 +139,11 @@ const Anmeldungen = () => {
           <div key={anmeldung.id} className="anmeldung-box">
             {editedAnmeldung && editedAnmeldung.id === anmeldung.id ? (
               <div>
-                <input type="text" name="vorname" value={editedAnmeldung.vorname} onChange={handleInputChange} />
-                <input type="text" name="nachname" value={editedAnmeldung.nachname} onChange={handleInputChange} />
-                <input type="number" name="anzahlPersonen" value={editedAnmeldung.anzahlPersonen} onChange={handleInputChange} />
-                <input type="text" name="menuAuswahl" value={editedAnmeldung.menuAuswahl} onChange={handleInputChange} />
-                <button onClick={handleSaveEdit}>Speichern</button>
+                <input type="text" name="vorname" value={editedAnmeldung.vorname} onChange={handleInputChangeAnmeldung} />
+                <input type="text" name="nachname" value={editedAnmeldung.nachname} onChange={handleInputChangeAnmeldung} />
+                <input type="number" name="anzahlPersonen" value={editedAnmeldung.anzahlPersonen} onChange={handleInputChangeAnmeldung} />
+                <input type="text" name="menuAuswahl" value={editedAnmeldung.menuAuswahl} onChange={handleInputChangeAnmeldung} />
+                <button onClick={handleSaveEditAnmeldung}>Speichern</button>
                 <button onClick={handleCancelEdit}>Abbrechen</button>
               </div>
             ) : (
@@ -123,9 +166,23 @@ const Anmeldungen = () => {
       <div className="trauung">
         {trauungenData.data && trauungenData.data.map((trauung) => (
           <div key={trauung.id} className="anmeldung-box">
-            <p>Vorname: {trauung.vorname}</p>
-            <p>Nachname: {trauung.nachname}</p>
-            <p>Anzahl Personen: {trauung.anzahlPersonen}</p>
+            {editedTrauung && editedTrauung.id === trauung.id ? (
+              <div>
+                <input type="text" name="vorname" value={editedTrauung.vorname} onChange={handleInputChangeTrauung} />
+                <input type="text" name="nachname" value={editedTrauung.nachname} onChange={handleInputChangeTrauung} />
+                <input type="number" name="anzahlPersonen" value={editedTrauung.anzahlPersonen} onChange={handleInputChangeTrauung} />
+                <button onClick={handleSaveEditTrauung}>Speichern</button>
+                <button onClick={handleCancelEdit}>Abbrechen</button>
+              </div>
+            ) : (
+              <div>
+                <p>Vorname: {trauung.vorname}</p>
+                <p>Nachname: {trauung.nachname}</p>
+                <p>Anzahl Personen: {trauung.anzahlPersonen}</p>
+                <button onClick={() => handleEditTrauung(trauung)}>Bearbeiten</button>
+                <button onClick={() => handleDeleteTrauung(trauung.id)}>Löschen</button>
+              </div>
+            )}
           </div>
         ))}
       </div>
